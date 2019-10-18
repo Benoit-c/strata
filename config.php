0be2c50b1db75d1c56441bf95ee0ae1923c2e163
benoit-c <colombet.benoit43@gmail.com>
2019-10-18 17:16:02 +0200

    commit

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$twitter = "https://twitter.com/DuvalMagic";
+$github = "https://github.com/Benoit-c/strata-benoit";
+$dribble = "https://dribbble.com/#";
+$envelop = "https://www.welcomeoffice.com/Infos/Fidelity.aspx?gclid=CjwKCAjwxaXtBRBbEiwAPqPxcFaZOdXZ3tZQFzkpV2y_3k-MhP780GkvghYrLHfXhk35j6mEaM1YWRoCIEoQAvD_BwE&gclsrc=aw.ds";
+$rue = "27 avenue des tilleuls 16000";
+$ville = "Linars";
+$pays = "France";
+$phone = "0606404075";
+$email = "email@gmail.com";
+?>
+
